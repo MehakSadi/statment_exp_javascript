64735a5a45842b54f0044897fb2d6bbeffcc0da1
Nazish <mehakstudio26@gmail.com>
2024-07-25 07:17:55 +0500

    statment_exp_javascript

---
+++ prmtive obj.js
@@ -0,0 +1,26 @@
+// Permitive data types : permitive datatype is the fundimental of datatype .. mean buldierof javascript . 
+// Thre are seve permitive datatype : NN SS BB U 
+// 1: Number , 2: Null , 3: Symbol , 4: String , 5:BigInt , 6: Boolean & 7: Undefine .
+
+let a = 367;
+let b = null; 
+let c = String ("God is Enough for my success"); 
+let d = "strong"
+let e = BigInt (340) + BigInt(350); 
+let f = true ;// if the result is 690 , the  result will be true otherwise false 
+let g 
+console.log(a, b ,c,d,e,f,g);
+console.log(typeof(d));
+
+
+// OBJECT : Object is non premitive datatype  mean key value pairs.
+
+const item ={
+BigInt: "true",
+ Success :"God is Enough for my success",
+  Lovish : "undefine",
+
+}
+
+console.log(item["Success"])
+console.log(item, typeof ('Lovish'));
